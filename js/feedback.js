c3a22ce4f157db7a0465da3a9033ab79f704c00d
0lumoR <romulolima950@gmail.com>
2025-09-25 08:07:09 -0300

    ajustes 2

--- js/feedback.js
+++ js/feedback.js
@@ -1,15 +1,5 @@
-// feedback.js
-const form = document.querySelector("form");
-
-form.addEventListener("submit", function (event) {
-  if (!form.checkValidity()) {
-    event.preventDefault(); // cancela envio só se inválido
-    event.stopPropagation();
-    alert("⚠️ Por favor, preencha todos os campos obrigatórios antes de enviar.");
-  }
-
-  form.classList.add("was-validated"); // efeito do Bootstrap
-});
-
+document.querySelector('form').onsubmit = function() {
+            alert('Seu formulário foi enviado com sucesso! Você será redirecionado em breve.');
+        };
 
 
